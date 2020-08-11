27e50070bb28ecf07627443f4c0cbd2993059e55
fouchtc <fouchtc@uw.edu>
2020-08-10 17:17:01 -0700

    init midterm deliverable

---
+++ scripts/chart-1.R
@@ -0,0 +1,28 @@
+library('dplyr')
+library('ggplot2')
+library('leaflet')
+library('shiny')
+
+csv <- read.csv('data/time_series_covid19_confirmed_US.csv', stringsAsFactors =
+                     FALSE)
+
+render_leaf_plot <- function(df){
+  covid_data <- df %>%
+    select(Province_State, Lat, Long_, X8.5.20, Combined_Key) %>%
+    filter(X8.5.20 > 100)
+
+  leaf_plot <- leaflet(data = covid_data) %>%
+    addProviderTiles('CartoDB.Positron') %>%
+    addCircles(
+      lat = ~Lat,
+      lng = ~Long_,
+      radius = ~(X8.5.20 / 1000),
+      popup =  ~paste(Combined_Key, '<br/>', 'Total cases: ', X8.5.20),
+      color = 'pink', fillOpacity = 0.7
+    ) %>%
+
+    addLayersControl(overlayGroups = c("Red","Blue") , baseGroups = c("background 1","background 2"),
+                     options = layersControlOptions(collapsed = FALSE))
+
+    return(leaf_plot)
+}

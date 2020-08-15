54ab1f4ab3c8c60c7462c8917ed47f6d4f91a643
Judy Nguyen <judy@Judys-MacBook-Pro.local>
2020-08-14 17:17:13 -0700

    Changed function for chart 2

--- scripts/chart-3.R
+++ scripts/chart-3.R
@@ -1,60 +1,35 @@
 library(plotly)
 library(dplyr)
 
-mask_data <- read.csv('data/mask-use-by-county.csv')
+mask_data <- read.csv('../data/mask-use-by-county.csv', stringsAsFactors = FALSE)
 
-#Create a new Dataset that contains top 10 counties
-render_chart3 <- function(mask_by_county) {
-  mask_by_county <- filter(mask_data,
-                                   c('Los Angeles',
-                                     'Cook',
-                                     'Harris',
-                                     'Maricopa',
-                                     'San Diego',
-                                     'Orange',
-                                     'Miami-Dade',
-                                     'Dallas',
-                                     'King',
-                                     'Queens'))
-  
-  BuildChart <- function(mask_by_countries, county){
-    
-    #y, x, and x1 values
-    x.counties <- c('Los Angeles',
-                     'Cook',
-                     'Harris',
-                     'Maricopa',
-                     'Riverside',
-                     'Orange',
-                     'Miami-Dade',
-                     'Dallas',
-                     'Kings',
-                     'Queens')
+codes <- c(06037, 
+           17031, 
+           48201, 
+           04013, 
+           06065, 
+           06059, 
+           12086, 
+           48113, 
+           36047, 
+           36081)
 
-  x1.codes <- c(06037, 
-                17031, 
-                48201, 
-                04013, 
-                06065, 
-                06059, 
-                12086, 
-                48113, 
-                36047, 
-                36081)
+counties <- c('Los Angeles',
+              'Cook',
+              'Harris',
+              'Maricopa',
+              'Riverside',
+              'Orange',
+              'Miami-Dade',
+              'Dallas',
+              'Kings',
+              'Queens')
 
-  y.freq <- c(0.786, 
-              0.722, 
-              0.736, 
-              0.734, 
-              0.803, 
-              0.754, 
-              0.756, 
-              0.757, 
-              0.732,
-              0.751)
+temp <- data.frame(codes, counties)
 
-#make chart using plotly
-mask_chart <- plot_ly(mask_data, x = ~county, y = ~percentage(), type = 'bar', 
+mask_data <- mask_data %>% filter(COUNTYFP %in% codes) %>% left_join(temp, by = c("COUNTYFP" = "codes"))
+
+mask_chart <- plot_ly(mask_data, x = ~counties, y = ~ALWAYS, type = 'bar', 
                       orientation = 'h',
                       marker = list(color = 'rgba(17, 132, 255, 0.6)',
                                     line = list(color = 'rgba(255, 180, 17, 1.0',
@@ -63,6 +38,3 @@ mask_chart <- plot_ly(mask_data, x = ~county, y = ~percentage(), type = 'bar',
          title = "Mask Usages in Top 10 U.S Counties",
          xaxis = list(title = ""),
          yaxis = list(title = ""))
-return(mask_chart)
-  }
-}

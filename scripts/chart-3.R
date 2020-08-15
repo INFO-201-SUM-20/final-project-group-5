38560b42b8d4c01f5f2dd0105335358f8d5ef8cd
fouchtc <fouchtc@uw.edu>
2020-08-14 17:32:05 -0700

    rendered chart-3 in markdown

--- scripts/chart-3.R
+++ scripts/chart-3.R
@@ -1,40 +1,57 @@
 library(plotly)
 library(dplyr)
 
-mask_data <- read.csv('../data/mask-use-by-county.csv', stringsAsFactors = FALSE)
+render_chart3 <- function() {
+  mask_data <- read.csv("data/mask-use-by-county.csv", stringsAsFactors = FALSE)
 
-codes <- c(06037, 
-           17031, 
-           48201, 
-           04013, 
-           06065, 
-           06059, 
-           12086, 
-           48113, 
-           36047, 
-           36081)
+  codes <- c(
+    06037,
+    17031,
+    48201,
+    04013,
+    06065,
+    06059,
+    12086,
+    48113,
+    36047,
+    36081
+  )
 
-counties <- c('Los Angeles',
-              'Cook',
-              'Harris',
-              'Maricopa',
-              'Riverside',
-              'Orange',
-              'Miami-Dade',
-              'Dallas',
-              'Kings',
-              'Queens')
+  counties <- c(
+    "Los Angeles",
+    "Cook",
+    "Harris",
+    "Maricopa",
+    "Riverside",
+    "Orange",
+    "Miami-Dade",
+    "Dallas",
+    "Kings",
+    "Queens"
+  )
 
-temp <- data.frame(codes, counties)
+  temp <- data.frame(codes, counties)
 
-mask_data <- mask_data %>% filter(COUNTYFP %in% codes) %>% left_join(temp, by = c("COUNTYFP" = "codes"))
+  mask_data <- mask_data %>%
+    filter(COUNTYFP %in% codes) %>%
+    left_join(temp, by = c("COUNTYFP" = "codes"))
 
-mask_chart <- plot_ly(mask_data, x = ~counties, y = ~ALWAYS, type = 'bar', 
-                      orientation = 'h',
-                      marker = list(color = 'rgba(17, 132, 255, 0.6)',
-                                    line = list(color = 'rgba(255, 180, 17, 1.0',
-                                                width = 1))) %>%
-  layout(barmode = 'stack',
-         title = "Mask Usages in Top 10 U.S Counties",
-         xaxis = list(title = ""),
-         yaxis = list(title = ""))
+  mask_chart <- plot_ly(mask_data,
+    x = ~counties, y = ~ALWAYS, type = "bar",
+    marker = list(
+      color = "rgba(17, 132, 255, 0.6)",
+      line = list(
+        color = "rgba(255, 180, 17, 1.0",
+        width = 1
+      )
+    )
+  ) %>%
+    layout(
+      barmode = "stack",
+      title = "Mask Usages in Top 10 U.S Counties",
+      xaxis = list(title = ""),
+      yaxis = list(title = "")
+    )
+
+  return(mask_chart)
+}

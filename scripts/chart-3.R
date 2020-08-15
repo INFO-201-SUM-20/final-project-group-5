be3c3e18ccc91c089950970345e069c785de45a0
Judy Nguyen <judy@Judys-MacBook-Pro.local>
2020-08-14 17:47:59 -0700

    Changed top county

--- scripts/chart-3.R
+++ scripts/chart-3.R
@@ -3,34 +3,26 @@ library(dplyr)
 
 render_chart3 <- function() {
   mask_data <- read.csv("data/mask-use-by-county.csv", stringsAsFactors = FALSE)
-
-  codes <- c(
-    06037,
-    17031,
-    48201,
-    04013,
-    06065,
-    06059,
-    12086,
-    48113,
-    36047,
-    36081
+  codes <- c(06037, 
+             17031, 
+             48201, 
+             04013, 
+             06065, 
+             06059, 
+             48113, 
+             36047)
+  
+  counties <- c('Los Angeles',
+                'Cook',
+                'Harris',
+                'Maricopa',
+                'Riverside',
+                'Orange',
+                'Dallas',
+                'Kings')
   )
 
-  counties <- c(
-    "Los Angeles",
-    "Cook",
-    "Harris",
-    "Maricopa",
-    "Riverside",
-    "Orange",
-    "Miami-Dade",
-    "Dallas",
-    "Kings",
-    "Queens"
-  )
-
-  temp <- data.frame(codes, counties)
+  top_5 <- data.frame(codes, counties)
 
   mask_data <- mask_data %>%
     filter(COUNTYFP %in% codes) %>%
@@ -48,7 +40,7 @@ render_chart3 <- function() {
   ) %>%
     layout(
       barmode = "stack",
-      title = "Mask Usages in Top 10 U.S Counties",
+      title = "Mask Usages in Top 5 U.S Counties",
       xaxis = list(title = ""),
       yaxis = list(title = "")
     )

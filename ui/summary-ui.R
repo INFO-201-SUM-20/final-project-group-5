4f34b20c535b9a01587f85d9f77e5ee4f10da71c
Judy Nguyen <judy@Judys-MacBook-Pro.local>
2020-08-21 12:47:01 -0700

    updated summary

--- ui/summary-ui.R
+++ ui/summary-ui.R
@@ -16,7 +16,11 @@ summary_ui <- fluidPage(
         and a high of 29,551.42. The S&P 500 has 
         reached a high of 3,386.15 and a low of 2,237.40. The highest positive rate 
         in a single day was 0.211 and the lowest tests per cases in a single day 
-        had reached 4.734.')
+        had reached 4.734.'),
 
+  h2('Patterns'),
+  
+  p('Looking at the bar chart, we can see that the most poplous counties in the 
+    User')
     )
   )
